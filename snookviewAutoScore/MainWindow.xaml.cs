a564a4109976f592ae9e4044e0d478a3fd56aaab
Gilko <gilles_vanpeteghem@hotmail.com>
2016-08-23 15:48:29 +0200

    4th Commit - Getting video source (Code in MainWindow.xaml.cs)

---
+++ snookviewAutoScore/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace snookviewAutoScore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        KinectSensor sensor;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+                sensor = KinectSensor.KinectSensors[0];
+            
+            if (sensor.Status == KinectStatus.Connected)
+            {
+                sensor.ColorStream.Enable();
+                sensor.DepthStream.Enable();
+                sensor.SkeletonStream.Enable();
+
+                //not for kinect for xbox 360, only for kinect for windows
+                /*sensor.DepthStream.Range = DepthRange.Near;
+                sensor.SkeletonStream.EnableTrackingInNearRange = true;
+                sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;*/
+
+                sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
+                sensor.Start();
+            }
+        }
+
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame == null)
+                    return;
+
+                byte[] pixels = new byte[colorFrame.PixelDataLength];
+                colorFrame.CopyPixelDataTo(pixels);
+
+                int stride = colorFrame.Width * 4;
+
+                vid.Source = BitmapSource.Create(colorFrame.Width, colorFrame.Height, 96,96, PixelFormats.Bgr32, null, pixels, stride);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            sensor.Stop();
+        }
+    }
+}
